f5c5d88b7557aba239872937668aee3fa6f45127
KrinaPatel0412 <kiyupatel2015@gmail.com>
2020-11-16 19:15:57 +0530

    MovieAdapter added

--- app/src/main/java/com/example/movies/models/Movie.java
+++ app/src/main/java/com/example/movies/models/Movie.java
@@ -2,16 +2,22 @@ package com.example.movies.models;
 
 public class Movie {
 
+    private int id;
     private String title;
     private Double ratings;
     private String description;
     private int image;
 
-    public Movie(String title, Double ratings, String description, int image) {
+    public Movie(int id, String title, Double ratings, String description, int image) {
+        this.id = id;
         this.title = title;
         this.ratings = ratings;
         this.description = description;
         this.image = image;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getTitle() {
